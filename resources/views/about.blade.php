bffe65c3bba4347daebe112da9b68a70c9de75a0
Daniel Palacios <rymdaniel@gmail.com>
2020-09-18 02:35:19 -0700

    Fixed types and wording in "About" page.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -22,25 +22,26 @@
                         This user experience is ruined, however, when the artists recommended have nothing to do with the user's behavior.
                     </p>
                     <p>
-                        Release Radar's purpose is to provide the latest music from artists a user follows, and additionally some singles picked <em>for you.</em>
-                        This playlist is generated weekly for Fridays, althought a new generated playlist is ready the night before at 9pm PST.
+                        Release Radar's purpose is to provide the latest music from artists a user follows, and additionally some similar artists' singles picked <em>for you.</em>
+                        This playlist is generated weekly for Fridays, although a new generated playlist is ready the night before at 9pm PST.
                     </p>
                     <p>
                         It makes sense that not every artist a user follows is going to put out new music every week, so sometimes the playlist will recommend music from <em>similar</em> artist. That however is not the issue.
-                        The issue with Release Radar is that it recommends music from artists you follow, but only at first glance.
+                        The issue with Release Radar is that it pollutes your playlist with artists who have the same name as some of those you <em>do</em> follow.
                     </p>
                     <p>
                         Let's choose a recognizable artist: The Beatles. We can assume if a user followed The Beatles on Spotify then they are following the English rock band from Liverpool. There aren't any other known groups by the exact same name but lets assume there was.
-                        Let's call The Beatles we all know <b>The_Beatles_1</b> and the other lesser known group <b>The_Beatles_2</b>. If a user happens to follow <b>The_Beatles_1</b>, and <b>The_Beatles_2</b> happens to have a new release,
-                        Release Radar fails to differentiate the two, and displays <b>The_Beatles_2's</b> new release, despite the user never following them.
+                        Let's call The Beatles we all know <b>"The Beatles 1"</b> and the other lesser known group <b>"The Beatles 2"</b>. If a user happens to follow <b>"The Beatles 1"</b>, and <b>"The Beatles 2"</b> happens to have a new release,
+                        Release Radar fails to differentiate the two, and displays the new release belonging to <b>"The Beatles 2"</b>, despite a user never following them.
                     </p>
                     <p>
-                        Release Radar is broken, and instead of searching for an artist by a unique identifier like an artist ID (something Spotify has access to already), Release Radar appears to be doing it by the name alone, (something that is never reliable and thus should never be done).
-                        This site aims to provide a better Release Radar, that <em>does</em> search for artists based on ID, at the expense of <em>similar artists</em>.
+                        Release Radar is broken, and instead of searching for an artist by a unique identifier like an artist ID (something Spotify has access to already), Release Radar appears to be doing it by artist name.
+                        This site aims to provide a better Release Radar, that <em>does</em> search for artists based on ID, at the expense of losing <em>similar artists</em>.
                     </p>
+                    <h4>Note</h4>
                     <p>
-                        There is an additional bug, however, when <b>The_Beatles_2's</b> new release appears on <b>The_Beatles_1's</b> artist page, but that's entirely in Spotify's hands, as they are in charge of that functionality.
-                        This application cannot take this issue into account as Spotify has failed to upload a song to the correct artist page.
+                        There is an additional bug where, for example, <b>"The Beatles 2"</b> new release appears on <b>"The Beatles 1"</b> artist page, but that's entirely in Spotify's hands, as they are in charge of that functionality.
+                        This application cannot take this issue into account as Spotify has failed to upload a song to the correct artist page. If you notice a wrong artist release by using this application, check the artist page first. The only scenario where that happens is when Spotify makes that error.
                     </p>
 
                     <hr class="mt-5">
@@ -49,12 +50,23 @@
                             <h2>List of API Endpoints Used</h2>
                             <ul>
                                 <li>
+                                    <h6>user-read-private</h6>
+                                    <p>Used to get your username, necessary to then save a playlist to your account.</p>
+                                </li>
+                                <li>
                                     <h6>user-follow-read</h6>
-                                    <p>Used to find every artist a user follows.</p>
+                                    <p>Used to find all the artists you are following.</p>
                                 </li>
                                 <li>
                                     <h6>playlist-modify-public</h6>
-                                    <p>Used to create a new "Better Release Radar" playlist for the user.</p>
+                                </li>
+                                <li>
+                                    <h6>playlist-modify-private</h6>
+                                    <p>Used to create and manage your "Better Release Radar" playlist on your account, whether it's set to public or private.</p>
+                                </li>
+                                <li>
+                                    <h6>ugc-image-upload</h6>
+                                    <p>Needed to upload your "Better Release Radar's" playlist cover image (You're free to change this image at any time).</p>
                                 </li>
                             </ul>
                         </div>
