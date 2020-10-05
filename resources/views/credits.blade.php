d43d5f936fc7334a2e54143eabd2c02e3a846061
Daniel Palacios <rymdaniel@gmail.com>
2020-10-04 20:13:48 -0700

    Updated credits page for playlist cover image and added missing "_blank" target attribute on all links.

--- resources/views/credits.blade.php
+++ resources/views/credits.blade.php
@@ -17,18 +17,19 @@
                     <li>
                         Vue.js
                         <ul>
-                            <li><a href="https://codepen.io/numerical/pen/yKMaaP">Vue.js + HTML5 Audio Player</a> by Cameron at CodePen</li>
+                            <li><a href="https://codepen.io/numerical/pen/yKMaaP" title="CodePen" target="_blank">Vue.js + HTML5 Audio Player</a> by Cameron at CodePen</li>
                         </ul>
                     </li>
                     <li>Spotify API</li>
                     <li>
                         Bootstrap
                         <ul>
-                            <li><a href="https://sharebootstrap.com/art-studio-minimal-studio-template/">Art Studio Minimal Portfolio Theme</a> by ShareBootstrap</li>
+                            <li><a href="https://sharebootstrap.com/art-studio-minimal-studio-template/" title="Art Studio Minimal Studio Template" target="_blank">Art Studio Minimal Portfolio Theme</a> by ShareBootstrap</li>
                         </ul>
                     </li>
-                    <li>Hamburger menu icon made by <a href="https://www.flaticon.com/authors/hirschwolf" title="hirschwolf">hirschwolf</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></li>
-                    <li>Potion icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></li>
+                    <li>Hamburger menu icon made by <a href="https://www.flaticon.com/authors/hirschwolf" title="hirschwolf" target="_blank">hirschwolf</a> from <a href="https://www.flaticon.com/" title="Flaticon" target="_blank">www.flaticon.com</a></li>
+                    <li>Potion icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik" target="_blank">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon" target="_blank">www.flaticon.com</a></li>
+                    <li><a href="https://www.pexels.com/photo/high-voltage-power-lines-against-mountains-in-sunset-4555345/" title="High Voltage Power Lines Against Mountains In Sunset" target="_blank">Playlist cover image</a> by <a href="https://www.pexels.com/@roman-odintsov" title="Roman Odintsov" target="_blank">ROMAN ODINTSOV</a> from <a href="https://www.pexels.com/" title="Pexels" target="_blank">Pexels</a></li>
                 </ul>
             </div>
         </div>
